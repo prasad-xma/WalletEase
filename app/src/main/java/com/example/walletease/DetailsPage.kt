65996cbd9b1b95bd549f35378fa14c29a1e4f618
Prasa-Xma <prasadbandara130@gmail.com>
2025-04-16 13:38:06 +0530

    create intent to register page

--- app/src/main/java/com/example/walletease/DetailsPage.kt
+++ app/src/main/java/com/example/walletease/DetailsPage.kt
@@ -1,6 +1,8 @@
 package com.example.walletease
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,11 @@ class DetailsPage : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val btnNavigateRegister = findViewById<TextView>(R.id.btnGetStart)
+        btnNavigateRegister.setOnClickListener{
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
         }
     }
 }
