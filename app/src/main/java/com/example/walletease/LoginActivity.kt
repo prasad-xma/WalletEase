de580d8dde26571b33092bc894b92952369c29b2
Prasa-Xma <prasadbandara130@gmail.com>
2025-04-16 21:36:25 +0530

    create login page frontend

--- app/src/main/java/com/example/walletease/LoginActivity.kt
+++ app/src/main/java/com/example/walletease/LoginActivity.kt
@@ -1,6 +1,8 @@
 package com.example.walletease
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,11 @@ class LoginActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val navToReg = findViewById<TextView>(R.id.navigateToReg)
+        navToReg.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
         }
     }
 }
