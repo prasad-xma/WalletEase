182aff89a8fd51ae835681258088c8e6316a1aeb
Prasa-Xma <prasadbandara130@gmail.com>
2025-04-16 11:45:21 +0530

    create splash screen

--- app/src/main/java/com/example/walletease/GetStarted.kt
+++ app/src/main/java/com/example/walletease/GetStarted.kt
@@ -1,31 +1,20 @@
 package com.example.walletease
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class GetStarted : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_get_started)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        supportActionBar?.hide()
-
-        Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this, GetStarted::class.java)
-            startActivity(intent)
-            finish()
-        }, 5000)
     }
 }
