7a775b11129cb98fecafe7e3352a13bda654ece6
Prasa-Xma <prasadbandara130@gmail.com>
2025-04-16 19:21:46 +0530

    create register page frontend

--- app/src/main/java/com/example/walletease/LoginActivity.kt
+++ app/src/main/java/com/example/walletease/LoginActivity.kt
@@ -1,28 +1,20 @@
 package com.example.walletease
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class RegisterActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_register)
+        setContentView(R.layout.activity_login)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val navigateToReg = findViewById<TextView>(R.id.tvNavigateLogin)
-        navigateToReg.setOnClickListener {
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
         }
     }
 }
