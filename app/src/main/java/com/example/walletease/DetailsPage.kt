ea2f4a4f8ca377b8b63520353c71c710ec80be46
Prasa-Xma <prasadbandara130@gmail.com>
2025-04-16 13:07:40 +0530

    create get started pages

--- app/src/main/java/com/example/walletease/DetailsPage.kt
+++ app/src/main/java/com/example/walletease/DetailsPage.kt
@@ -1,29 +1,20 @@
 package com.example.walletease
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class GetStarted : AppCompatActivity() {
-
+class DetailsPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_get_started)
+        setContentView(R.layout.activity_details_page)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val nextBtn = findViewById<TextView>(R.id.textView2)
-        nextBtn.setOnClickListener{
-            val intent = Intent(this, DetailsPage::class.java)
-            startActivity(intent)
         }
     }
 }
