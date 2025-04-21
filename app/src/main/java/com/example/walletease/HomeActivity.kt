8058344fb5884fb206664ef6ed9d449a0d5ddfae
Prasa-Xma <prasadbandara130@gmail.com>
2025-04-21 12:08:29 +0530

    add logout function

--- app/src/main/java/com/example/walletease/HomeActivity.kt
+++ app/src/main/java/com/example/walletease/HomeActivity.kt
@@ -1,5 +1,6 @@
 package com.example.walletease
 
+import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.TextView
@@ -12,6 +13,7 @@ class HomeActivity : AppCompatActivity() {
 
     private lateinit var tvWelcome : TextView
     private lateinit var sharedPreferences :SharedPreferences
+    private lateinit var btnLogout: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,10 +25,15 @@ class HomeActivity : AppCompatActivity() {
             insets
         }
 
+        btnLogout = findViewById(R.id.tvBtnLogout)
         tvWelcome = findViewById(R.id.tvWelcomeMsg)
         sharedPreferences = getSharedPreferences("user_profile", MODE_PRIVATE)
 
         displayWelcomeMessage()
+
+        btnLogout.setOnClickListener {
+            logoutUser()
+        }
     }
 
     private fun displayWelcomeMessage(){
@@ -34,5 +41,15 @@ class HomeActivity : AppCompatActivity() {
         val uEmail = sharedPreferences.getString("email", "")
 
         tvWelcome.text = "Welcome, $uName\n$uEmail"
+    }
+
+    private fun logoutUser() {
+        val editor = sharedPreferences.edit()
+        editor.putBoolean("isLoggedIn", false)
+        editor.apply()
+
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
