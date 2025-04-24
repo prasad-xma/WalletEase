43d50f910740338af42831b6a893fe384ec95f94
Prasa-Xma <prasadbandara130@gmail.com>
2025-04-24 20:15:33 +0530

    fixed the errors

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,7 @@ android {
 }
 
 dependencies {
-    implementation(libs.mpandroidchart)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
@@ -45,4 +45,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.mpandroidchart)
 }
