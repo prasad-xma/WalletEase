aa4208f4c4fe586fc95e55052e71a4295a1514b2
Prasa-Xma <prasadbandara130@gmail.com>
2025-04-24 19:46:11 +0530

    add nav bar

--- app/src/main/java/com/example/walletease/HistoryActivity.kt
+++ app/src/main/java/com/example/walletease/HistoryActivity.kt
@@ -1,12 +1,20 @@
 package com.example.walletease
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class HistoryActivity : AppCompatActivity() {
+
+    // bottom navigation
+    private lateinit var bottomNavHome: LinearLayout
+    private lateinit var bottomNavAction: LinearLayout
+    private lateinit var bottomNavHistory: LinearLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +24,20 @@ class HistoryActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // bottom navigation
+        bottomNavHome = findViewById(R.id.bottomNavHome)
+        bottomNavHome.setOnClickListener {
+            startActivity(Intent(this, HomeActivity::class.java))
+        }
+        bottomNavAction = findViewById(R.id.bottomNavAction)
+        bottomNavAction.setOnClickListener {
+            startActivity(Intent(this, TransactionActivity::class.java))
+        }
+        bottomNavHistory = findViewById(R.id.bottomNavHistory)
+        bottomNavHistory.setOnClickListener {
+            startActivity(Intent(this, HistoryActivity::class.java))
+        }
+
     }
 }
